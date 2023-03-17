5e37bd65301fcd13e8f62d20ecbfbd31bf6edb4d
harrysudanaa <komangharry02@gmail.com>
2023-03-17 18:01:02 +0800

    Change category layout

--- resources/views/livewire/components/category/category.blade.php
+++ resources/views/livewire/components/category/category.blade.php
@@ -1,35 +1,3 @@
 <div>
-    <div class="mx-auto w-[75%] mb-20 border rounded">
-        {{-- nav category --}}
-        <div class="rounded-lg">
-            <ul class="flex justify-around py-3 rounded shadow-md border">
-                <li class="bg-merah px-3 py-1 my-1 rounded text-white">Category</li>
-                <li class="bg-merah px-3 py-1 my-1 rounded text-white">Category</li>
-                <li class="bg-merah px-3 py-1 my-1 rounded text-white">Category</li>
-                <li class="bg-merah px-3 py-1 my-1 rounded text-white">Category</li>
-                <li class="bg-merah px-3 py-1 my-1 rounded text-white">Category</li>
-            </ul>
-        </div>
-        {{-- card --}}
-        <div class="my-5">
-            <div class="w-[75%] flex flex-wrap justify-around mx-auto">
-                <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-                    alt="" width="150" class="mx-5 my-5">
-                <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-                    alt="" width="150" class="mx-5 my-5">
-                <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-                    alt="" width="150" class="mx-5 my-5">
-                <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-                    alt="" width="150" class="mx-5 my-5">
-                <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-                    alt="" width="150" class="mx-5 my-5">
-                <img src="https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-                    alt="" width="150" class="mx-5 my-5">
-            </div>
-        </div>
-        {{-- button more --}}
-        <div class="my-5 flex justify-center">
-            <button class="bg-merah shadow rounded px-3 py-1 text-white">Load more...</button>
-        </div>
-    </div>
+    <a href="">tes link</a>
 </div>
